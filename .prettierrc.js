ffb3e2cde9c7023b6bf991dfeb7d6b1f397158aa
pch1024 <pch1024@outlook.com>
2023-11-24 23:18:03 +0800

    deploy
    1.0.44
    
    deploy
    
    1.0.45
    
    deploy
    
    1.0.46

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,8 +2,8 @@ module.exports = {
 	useTabs: true, // 采用tab缩进还是空白缩进
 	tabWidth: 4, // Tab键宽度
 	printWidth: 120, // 宽度限制
-	singleQuote: true, //字符串是否使用单引号
-	semi: true, //行位是否使用分号
-	trailingComma: 'none', //是否使用尾逗号
+	singleQuote: true, // 字符串是否使用单引号
+	semi: true, // 行位是否使用分号
+	trailingComma: 'none', // 是否使用尾逗号
 	bracketSpacing: true // 是否保留括号中的空格 默认true
 };
